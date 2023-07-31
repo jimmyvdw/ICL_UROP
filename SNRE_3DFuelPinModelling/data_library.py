95b5be160ee7280bd7740508a8ffd2cfae1264e9
jimmyvdw <jv20@ic.ac.uk>
2023-07-31 10:32:07 +0100

    scripts

---
+++ SNRE_3DFuelPinModelling/data_library.py
@@ -0,0 +1,93 @@
+import openmc
+
+library = openmc.data.DataLibrary()
+
+u235 = openmc.data.IncidentNeutron.from_ace('./dataLibrary/U235/92235ENDF7300.ace')
+for suffix in ['300', '600', '900', '1200', '1500', '1800']:
+    u235.add_temperature_from_ace('./dataLibrary/U235/92235ENDF7{}.ace'.format(suffix))
+u235.export_to_hdf5('./dataLibrary/U235/U235.h5')
+library.register_file('./dataLibrary/U235/U235.h5')
+
+u238 = openmc.data.IncidentNeutron.from_ace('./dataLibrary/U238/92238ENDF7300.ace')
+for suffix in ['300', '600', '900', '1200', '1500', '1800']:
+    u238.add_temperature_from_ace('./dataLibrary/U238/92238ENDF7{}.ace'.format(suffix))
+u238.export_to_hdf5('./dataLibrary/U238/U238.h5')
+library.register_file('./dataLibrary/U238/U238.h5')
+
+h1 = openmc.data.IncidentNeutron.from_ace('./dataLibrary/H1/1001ENDF7300.ace')
+for suffix in ['300', '600', '900', '1200', '1500', '1800']:
+    h1.add_temperature_from_ace('./dataLibrary/H1/1001ENDF7{}.ace'.format(suffix))
+h1.export_to_hdf5('./dataLibrary/H1/H1.h5')
+library.register_file('./dataLibrary/H1/H1.h5')
+
+h2 = openmc.data.IncidentNeutron.from_ace('./dataLibrary/H2/1002ENDF7300.ace')
+for suffix in ['300', '600', '900', '1200', '1500', '1800']:
+    h2.add_temperature_from_ace('./dataLibrary/H2/1002ENDF7{}.ace'.format(suffix))
+h2.export_to_hdf5('./dataLibrary/H2/H2.h5')
+library.register_file('./dataLibrary/H2/H2.h5')
+
+h3 = openmc.data.IncidentNeutron.from_ace('./dataLibrary/H3/1003ENDF7300.ace')
+for suffix in ['300', '600', '900', '1200', '1500', '1800']:
+    h3.add_temperature_from_ace('./dataLibrary/H3/1003ENDF7{}.ace'.format(suffix))
+h3.export_to_hdf5('./dataLibrary/H3/H3.h5')
+library.register_file('./dataLibrary/H3/H3.h5')
+
+c12 = openmc.data.IncidentNeutron.from_ace('./dataLibrary/C12/6000ENDF7300.ace')
+for suffix in ['300', '600', '900', '1200', '1500', '1800']:
+    c12.add_temperature_from_ace('./dataLibrary/C12/6000ENDF7{}.ace'.format(suffix))
+c12.export_to_hdf5('./dataLibrary/C12/C12.h5')
+library.register_file('./dataLibrary/C12/C12.h5')
+
+zr90 = openmc.data.IncidentNeutron.from_ace('./dataLibrary/Zr90/40090ENDF7300.ace')
+for suffix in ['300', '600', '900', '1200', '1500', '1800']:
+    zr90.add_temperature_from_ace('./dataLibrary/Zr90/40090ENDF7{}.ace'.format(suffix))
+zr90.export_to_hdf5('./dataLibrary/Zr90/Zr90.h5')
+library.register_file('./dataLibrary/Zr90/Zr90.h5')
+
+zr91 = openmc.data.IncidentNeutron.from_ace('./dataLibrary/Zr91/40091ENDF7300.ace')
+for suffix in ['300', '600', '900', '1200', '1500', '1800']:
+    zr91.add_temperature_from_ace('./dataLibrary/Zr91/40091ENDF7{}.ace'.format(suffix))
+zr91.export_to_hdf5('./dataLibrary/Zr91/Zr91.h5')
+library.register_file('./dataLibrary/Zr91/Zr91.h5')
+
+zr92 = openmc.data.IncidentNeutron.from_ace('./dataLibrary/Zr92/40092ENDF7300.ace')
+for suffix in ['300', '600', '900', '1200', '1500', '1800']:
+    zr92.add_temperature_from_ace('./dataLibrary/Zr92/40092ENDF7{}.ace'.format(suffix))
+zr92.export_to_hdf5('./dataLibrary/Zr92/Zr92.h5')
+library.register_file('./dataLibrary/Zr92/Zr92.h5')
+
+zr93 = openmc.data.IncidentNeutron.from_ace('./dataLibrary/Zr93/40093ENDF7300.ace')
+for suffix in ['300', '600', '900', '1200', '1500', '1800']:
+    zr93.add_temperature_from_ace('./dataLibrary/Zr93/40093ENDF7{}.ace'.format(suffix))
+zr93.export_to_hdf5('./dataLibrary/Zr93/Zr93.h5')
+library.register_file('./dataLibrary/Zr93/Zr93.h5')
+
+zr94 = openmc.data.IncidentNeutron.from_ace('./dataLibrary/Zr94/40094ENDF7300.ace')
+for suffix in ['300', '600', '900', '1200', '1500', '1800']:
+    zr94.add_temperature_from_ace('./dataLibrary/Zr94/40094ENDF7{}.ace'.format(suffix))
+zr94.export_to_hdf5('./dataLibrary/Zr94/Zr94.h5')
+library.register_file('./dataLibrary/Zr94/Zr94.h5')
+
+zr95 = openmc.data.IncidentNeutron.from_ace('./dataLibrary/Zr95/40095ENDF7300.ace')
+for suffix in ['300', '600', '900', '1200', '1500', '1800']:
+    zr95.add_temperature_from_ace('./dataLibrary/Zr95/40095ENDF7{}.ace'.format(suffix))
+zr95.export_to_hdf5('./dataLibrary/Zr95/Zr95.h5')
+library.register_file('./dataLibrary/Zr95/Zr95.h5')
+
+zr96 = openmc.data.IncidentNeutron.from_ace('./dataLibrary/Zr96/40096ENDF7300.ace')
+for suffix in ['300', '600', '900', '1200', '1500', '1800']:
+    zr96.add_temperature_from_ace('./dataLibrary/Zr96/40096ENDF7{}.ace'.format(suffix))
+zr96.export_to_hdf5('./dataLibrary/Zr96/Zr96.h5')
+library.register_file('./dataLibrary/Zr96/Zr96.h5')
+
+c_para_H = openmc.data.ThermalScattering.from_ace('./dataLibrary/c_para_H/hpara.10t')
+c_para_H.export_to_hdf5('./dataLibrary/c_para_H/c_para_H.h5')
+library.register_file('./dataLibrary/c_para_H/c_para_H.h5')
+
+c_Graphite = openmc.data.ThermalScattering.from_ace('./dataLibrary/c_Graphite/grph.10t')
+for suffix in ['10', '11', '12', '13', '14', '15', '16', '17', '18', '19']:
+    c_Graphite.add_temperature_from_ace('./dataLibrary/c_Graphite/grph.{}t'.format(suffix))
+c_Graphite.export_to_hdf5('./dataLibrary/c_Graphite/c_Graphite.h5')
+library.register_file('./dataLibrary/c_Graphite/c_Graphite.h5')
+
+library.export_to_xml('./dataLibrary/cross_sections.xml')
