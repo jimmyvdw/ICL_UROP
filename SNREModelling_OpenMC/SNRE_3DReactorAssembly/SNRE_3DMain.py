0ed96bc3d34908e7cd2b99addbece71dcf155275
jimmyvdw <jv20@ic.ac.uk>
2023-08-17 08:33:45 +0100

    long run 90 deg of both models

--- SNREModelling_OpenMC/SNRE_3DReactorAssembly/SNRE_3DMain.py
+++ SNREModelling_OpenMC/SNRE_3DReactorAssembly/SNRE_3DMain.py
@@ -24,9 +24,9 @@ def SNRE_3DMain():
 
     settings.source = source
 
-    settings.particles = 2000
+    settings.particles = 100000
     settings.generations_per_batch = 20
-    batches = settings.batches = 40
+    batches = settings.batches = 100
     settings.inactive = 20
     # settings.trace = (20,10,500)
     # settings.track = [(20,10,500)]
